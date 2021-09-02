7a47dec3b3a3923c8afce228f40800a6802a95c7
dirkan <87771600+dirkanCODE@users.noreply.github.com>
2021-09-02 11:25:38 +0200

    Update ComandosDA-PS.ps1

--- Scripts DA/ComandosDA-PS.ps1
+++ Scripts DA/ComandosDA-PS.ps1
@@ -1,5 +1,5 @@
 # Obtener usuarios logados en los últimos 30 Días
 
 Get-ADUser -Filter * -Properties LastLogonDate | where {$_.LastLogonDate -le (Get-Date).AddDays(-30)} | sort-object -property lastlogondate -descending | Format-Table -property name, lastlogondate -AutoSize
----------------------------------------------------------------------------
+
 
