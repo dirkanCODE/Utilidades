8bd5f72ea9508c961de81abae7342b40ce7f7735
dirkan <87771600+dirkanCODE@users.noreply.github.com>
2021-09-08 10:01:50 +0200

    Update ComandosDA-PS.ps1

--- Scripts DA/ComandosDA-PS.ps1
+++ Scripts DA/ComandosDA-PS.ps1
@@ -1,10 +1,10 @@
 #By: dirkan (@dirkanSYN) -> https://twitter.com/dirkanSYN
 
-**# Obtener usuarios logados en los últimos 30 Días
+** # Obtener usuarios logados en los últimos 30 Días
 
 Get-ADUser -Filter * -Properties LastLogonDate | where {$_.LastLogonDate -le (Get-Date).AddDays(-30)} | sort-object -property lastlogondate -descending | Format-Table -property name, lastlogondate -AutoSize
 
-**# Buscar Objetos Modificados en DA entre dos Fechas-Horas
+** # Buscar Objetos Modificados en DA entre dos Fechas-Horas
 
 $changeDateStart = New-Object DateTime(2021, 03, 26, 10, 00, 00)
 $changeDateEnd = New-Object DateTime(2021, 03, 26, 16, 00, 00)
